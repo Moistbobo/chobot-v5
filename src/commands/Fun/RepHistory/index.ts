519df6db0ba1e2dad13ae9975ffecb7e93d194d0
Kevin Pang <moistbobo@gmail.com>
2022-05-25 01:08:33 +0800

    fix: a big no no

--- src/commands/Fun/RepHistory/index.ts
+++ src/commands/Fun/RepHistory/index.ts
@@ -6,7 +6,7 @@ const RepHistory: Command = {
   name: 'Reputation History',
   triggers: ['rephistory', 'rh'],
   description: 'Shows the reputation history for the specified user. Defaults to sender.',
-  exampleUsage: `${envConfig.BOT_TOKEN}rh [username]\n${envConfig.BOT_TOKEN}rh moistbobo\n${envConfig.BOT_TOKEN}rh`,
+  exampleUsage: `${envConfig.BOT_PREFIX}rh [username]\n${envConfig.BOT_PREFIX}rh moistbobo\n${envConfig.BOT_PREFIX}rh`,
 };
 
 export default RepHistory;
