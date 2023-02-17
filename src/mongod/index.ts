56e1cf505bacd42c5bee0775d8cc395e40539383
Kevin Pang <moistbobo@gmail.com>
2023-02-18 00:35:45 +0800

    chore: use connection string

--- src/mongod/index.ts
+++ src/mongod/index.ts
@@ -1,18 +1,13 @@
 import Mongoose from 'mongoose';
 import envConfig from 'config/envConfig';
 
-const db = 'mongodb://localhost/choggabot';
+const db = `mongodb://${envConfig.MONGO_DB_USERNAME}:${envConfig.MONGO_DB_PASSWORD}@localhost/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256`;
 
 const connect = () =>
   Mongoose.connect(db, {
     // @ts-ignore
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    authMechanism: 'SCRAM-SHA-256',
-    auth: {
-      username: envConfig.MONGO_DB_USERNAME,
-      password: envConfig.MONGO_DB_PASSWORD,
-    },
   });
 
 const disconnect = () => Mongoose.disconnect();
