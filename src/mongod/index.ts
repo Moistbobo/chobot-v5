a150dae56c9c0f7d9bbf14c78c13799227556d55
Kevin Pang <moistbobo@gmail.com>
2023-02-18 00:39:55 +0800

    chore: add debug string

--- src/mongod/index.ts
+++ src/mongod/index.ts
@@ -1,6 +1,7 @@
 import Mongoose from 'mongoose';
 import envConfig from 'config/envConfig';
 
+console.log('Connecting to mongod with:', envConfig.MONGO_CONNECTION_URI);
 const db = envConfig.MONGO_CONNECTION_URI;
 
 const connect = () =>
