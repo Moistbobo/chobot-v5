007a4be011bfe94c79d5c1cac5988f8199377394
Kevin Pang <moistbobo@gmail.com>
2023-05-18 20:12:48 +0800

    chore: add default db connection

--- src/config/envConfig.ts
+++ src/config/envConfig.ts
@@ -4,7 +4,7 @@ const envConfig = {
   BOT_TOKEN: process.env.BOT_TOKEN,
   BOT_PREFIX: process.env.BOT_PREFIX || '.',
   NADEKO_DB_PATH: process.env.NADEKO_DB_PATH || '',
-  MONGO_CONNECTION_URI: process.env.MONGO_CONNECTION_URI || '',
+  MONGO_CONNECTION_URI: process.env.MONGO_CONNECTION_URI || 'mongodb://localhost:27017/choggabot',
 };
 
 export default envConfig;
